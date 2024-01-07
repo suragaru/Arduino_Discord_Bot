6d8f5621b0fa4dd4142f5b43af405b381bcb334f
yobick <155812618+yoobick@users.noreply.github.com>
2024-01-07 16:21:28 +0800

    Create pyserial.py

---
+++ pyserial.py
@@ -0,0 +1,24 @@
+import serial
+import time
+
+serialcomm = serial.Serial('COM7', 9600)
+
+serialcomm.timeout = 1
+
+while True:
+
+    i = input("Enter Input: ").strip()
+
+    if i == "Done":
+
+        print('finished')
+
+        break
+
+    serialcomm.write(i.encode())
+
+    time.sleep(0.5)
+
+    print(serialcomm.readline().decode('ascii'))
+
+serialcomm.close()
